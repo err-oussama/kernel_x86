1e785c464c067a05b8f62cfd04e341406d6b44c0
oussama errami <oussama0errami@gmail.com>
2026-04-26 02:30:45 +0100

    style: change the name of S type in gdt and add some type of GDT descriptor if S it is system

--- kernel/gdt.c
+++ kernel/gdt.c
@@ -38,43 +38,42 @@ void setup_gdt_entrys(gdt_entry *entrys) {
   // setup kernel code descriptor
   gdt_set_entry(&entrys[1], 0, 0xffff,
                 GDT_ACC_ACCESSED | GDT_ACC_EXEC_READ | GDT_ACC_NON_CONFORMING |
-                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_TYPE_CODE_DATA |
-                    GDT_ACC_DPL0 | GDT_ACC_PRESENT,
+                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_S_SEGMENT | GDT_ACC_DPL0 |
+                    GDT_ACC_PRESENT,
                 GDT_FLAG_AVL_0 | GDT_FLAG_64BIT | GDT_FLAG_OP_SIZE_16 |
                     GDT_FLAG_SEG_UNIT_4KB);
   // setup kernel data descriptor
   gdt_set_entry(&entrys[2], 0, 0xffff,
                 GDT_ACC_ACCESSED | GDT_ACC_READ_WRITE | GDT_ACC_GROW_UP |
-                    GDT_ACC_TYPE_DATA_SEG | GDT_ACC_TYPE_CODE_DATA |
-                    GDT_ACC_DPL0 | GDT_ACC_PRESENT,
+                    GDT_ACC_TYPE_DATA_SEG | GDT_ACC_S_SEGMENT | GDT_ACC_DPL0 |
+                    GDT_ACC_PRESENT,
                 GDT_FLAG_AVL_0 | GDT_FLAG_32BIT | GDT_FLAG_OP_SIZE_32 |
                     GDT_FLAG_SEG_UNIT_4KB);
   // setup user code 32-bit mode descriptor
   gdt_set_entry(&entrys[3], 0, 0xffff,
                 GDT_ACC_ACCESSED | GDT_ACC_EXEC_READ | GDT_ACC_NON_CONFORMING |
-                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_TYPE_CODE_DATA |
-                    GDT_ACC_DPL3 | GDT_ACC_PRESENT,
+                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_S_SEGMENT | GDT_ACC_DPL3 |
+                    GDT_ACC_PRESENT,
                 GDT_FLAG_AVL_0 | GDT_FLAG_32BIT | GDT_FLAG_OP_SIZE_32 |
                     GDT_FLAG_SEG_UNIT_4KB);
   // setup user data descriptor
   gdt_set_entry(&entrys[4], 0, 0xffff,
                 GDT_ACC_ACCESSED | GDT_ACC_READ_WRITE | GDT_ACC_GROW_UP |
-                    GDT_ACC_TYPE_DATA_SEG | GDT_ACC_TYPE_CODE_DATA |
-                    GDT_ACC_DPL3 | GDT_ACC_PRESENT,
+                    GDT_ACC_TYPE_DATA_SEG | GDT_ACC_S_SEGMENT | GDT_ACC_DPL3 |
+                    GDT_ACC_PRESENT,
                 GDT_FLAG_AVL_0 | GDT_FLAG_32BIT | GDT_FLAG_OP_SIZE_32 |
                     GDT_FLAG_SEG_UNIT_4KB);
   // setup user code 64-bit mode descriptor
   gdt_set_entry(&entrys[5], 0, 0xffff,
                 GDT_ACC_ACCESSED | GDT_ACC_EXEC_READ | GDT_ACC_NON_CONFORMING |
-                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_TYPE_CODE_DATA |
-                    GDT_ACC_DPL3 | GDT_ACC_PRESENT,
+                    GDT_ACC_TYPE_CODE_SEG | GDT_ACC_S_SEGMENT | GDT_ACC_DPL3 |
+                    GDT_ACC_PRESENT,
                 GDT_FLAG_AVL_0 | GDT_FLAG_64BIT | GDT_FLAG_OP_SIZE_16 |
                     GDT_FLAG_SEG_UNIT_4KB);
   // setup TSS descriptor
   gdt_set_TSS_descriptor(&entrys[6], 0, 0x67,
                          GDT_ACC_ACCESSED | GDT_ACC_NON_CONFORMING |
                              GDT_ACC_READ_ONLY | GDT_ACC_TYPE_CODE_SEG |
-                             GDT_ACC_TYPE_KERN_DATA | GDT_ACC_DPL0 |
-                             GDT_ACC_PRESENT,
+                             GDT_ACC_S_SYSTEM | GDT_ACC_DPL0 | GDT_ACC_PRESENT,
                          0x0);
 }
